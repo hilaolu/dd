1b8506d6fa428fc9509d84933aa649831ba54a85
hilaolu <i@phyzait.moe>
2020-12-19 18:45:44 +0800

    Update InstallNET.sh

--- InstallNET.sh
+++ InstallNET.sh
@@ -279,7 +279,7 @@ if [[ -z "$DIST" ]]; then
         [[ "$isDigital" == '12.04' ]] && DIST='precise';
         [[ "$isDigital" == '14.04' ]] && DIST='trusty';
         [[ "$isDigital" == '16.04' ]] && DIST='xenial';
-        [[ "$isDigital" == '18.04' ]] && DIST='bionic';
+        [[ "$isDigital" == '18.04' ]] && DIST='focal';
       }
     }
     LinuxMirror=$(SelectMirror "$Relese" "$DIST" "$VER" "$tmpMirror")
